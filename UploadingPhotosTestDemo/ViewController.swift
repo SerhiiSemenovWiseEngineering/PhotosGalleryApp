0f19c660018a2a65057483d559b3199da7e02999
Serhii Semenov <>
2022-03-24 12:13:45 +0200

    MOB-1001 First Commit

---
+++ UploadingPhotosTestDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UploadingPhotosTestDemo
+//
+//  Created by Serhii Semenov on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
